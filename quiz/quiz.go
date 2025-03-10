ef4d36904da284658972484be929e532eb654240
Ashish <ashish.tiwari2@ingka.ikea.com>
2025-03-10 20:45:30 +0100

    Add timer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -4,7 +4,9 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand/v2"
 	"os"
+	"time"
 )
 
 type problem struct {
@@ -15,6 +17,7 @@ type problem struct {
 func main() {
 
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of questions,answer")
+	timeLimit := flag.Int("limit", 15, "time limit for the quiz to complete in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -30,19 +33,26 @@ func main() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem no. %d: %s = \n", i+1, p.question)
-		var answer string
-		_, err = fmt.Scanf("%s\n", &answer)
-		if err != nil {
-			fmt.Println("Failed to read the answer", err)
-		}
-		if answer == p.answer {
-			correct++
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %d out of %d. \n", correct, len(problems))
+			return
+		default:
+			fmt.Printf("Problem no. %d: %s = \n", i+1, p.question)
+			var answer string
+			_, err = fmt.Scanf("%s\n", &answer)
+			if err != nil {
+				fmt.Println("Failed to read the answer", err)
+			}
+			if answer == p.answer {
+				correct++
+			}
 		}
 	}
-
 	fmt.Printf("You scored %d out of %d. \n", correct, len(problems))
 }
 
@@ -54,5 +64,8 @@ func parseLines(lines [][]string) []problem {
 			answer:   line[1],
 		}
 	}
+	rand.Shuffle(len(problems), func(i, j int) {
+		problems[i], problems[j] = problems[j], problems[i]
+	})
 	return problems
 }
